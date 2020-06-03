a5003974b6d2c5758786b2b395374fdbad47b5a5
weichen <17621645202@163.com>
2020-06-03 22:54:57 +0800

    fix:release 0606

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './views/app'
-// text git flow feature 0606 release
+// text git flow feature 0606 release --
 ReactDOM.render(<App />, document.getElementById('root'));
