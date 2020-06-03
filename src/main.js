96c10ded1f451a73d43658f9c9db7795b2019de7
weichen <17621645202@163.com>
2020-06-03 23:21:16 +0800

    fix:0610

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './views/app'
-// text git flow feature 0609
+// text git flow feature 0610
 ReactDOM.render(<App />, document.getElementById('root'));
